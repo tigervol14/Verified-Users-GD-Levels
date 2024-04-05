86c1495515152a38f9447b60ec25b45137b6f449
tigervol14 <99061416+tigervol14@users.noreply.github.com>
2024-04-04 20:33:32 -0400

    new thing idk

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,7 @@ bool is_mod;
 	}
 
 	void found_youtube(CCObject*) {
-		FLAlertLayer::create("Verified User Found!","This user is a <cr>prominent member</c> of the <cy>Geometry Dash</c> Community!","OK")->show();
+		FLAlertLayer::create("Verified User Found!","This user is a <cg>Verified Thumbnailer</c> in the <cl>Level Thumbnails</c> Discord Server!","OK")->show();
 		if (is_mod) m_fields->icon->setScale(m_usernameLabel->getScale() + 0.05f);
 		else m_fields->icon->setScale(m_usernameLabel->getScale() + 0.1f);
 	}
